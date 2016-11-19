39f0529103cfc418918ad301616e9f1d3fa3cea9
weolar <weolar@qq.com>
2016-11-19 11:45:30 +0800

    * 修复img标签，如果既有src又有background: url，导致无法显示src的bug。 bug触发的条件是src使用了一像素大小的透明png。 bug的原因是使用了gdip来解析png，这会先转换成bmp再丢给默认的bmpdecoder来解码，但忘记了设置ImageFrame::setHasAlpha。这在LayoutImage::foregroundIsKnownToBeOpaqueInRect会导致blink认为src的image是不透明的，这样就不用绘制真实的background。在map.qq.com会出现此bug。 在修复此bug的同时，还重构了gdpp解码方式。以前的方式要转成bmp，这个样效率太低。现在直接gdip解码后送入ImageFrame。但目前还不完美，是每个像素的设置。以后有时间改成一段段的设置

---
+++ third_party/WebKit/Source/platform/image-decoders/GDIPlus/GDIPlusReader.cpp
@@ -0,0 +1,89 @@
+
+#include "config.h"
+#include <windows.h>
+
+#include "platform/image-decoders/GDIPlus/GDIPlusReader.h"
+
+#undef min
+#undef max
+using std::max;
+using std::min;
+#include <gdiplus.h>
+
+namespace blink {
+
+GDIPlusReader::GDIPlusReader(ImageDecoder* parent, Gdiplus::Bitmap* gdipBitmap)
+    : m_parent(parent)
+    , m_buffer(0)
+    , m_gdipBitmap(gdipBitmap)
+{
+
+}
+
+GDIPlusReader::~GDIPlusReader()
+{
+    delete m_gdipBitmap;
+    m_gdipBitmap = nullptr;
+}
+
+static void fillbitmap(Gdiplus::Bitmap* gdipBitmap, ImageFrame* buffer)
+{
+    UINT w = gdipBitmap->GetWidth();
+    UINT h = gdipBitmap->GetHeight();
+
+    Gdiplus::Rect rect(0, 0, w, h);
+    Gdiplus::BitmapData lockedBitmapData;
+    gdipBitmap->LockBits(&rect, Gdiplus::ImageLockModeRead, PixelFormat32bppARGB, &lockedBitmapData);
+
+    unsigned int *pData = reinterpret_cast<unsigned int *>(lockedBitmapData.Scan0);
+    int stride = lockedBitmapData.Stride;
+
+    UINT hasAlpha = 0xff000000;
+    for (UINT x = 0; x < w; ++x) {
+        for (UINT y = 0; y < h; ++y) {
+            UINT color = pData[y * stride / 4 + x]; // color= 0xAARRGGBB
+            hasAlpha &= color;
+            UINT red = GetRValue(color);
+            UINT green = GetGValue(color);
+            UINT blue = GetBValue(color);
+            UINT alpha = (LOBYTE((color) >> 24));
+            buffer->setRGBA(x, y, red, green, blue, alpha);
+        }
+    }
+
+    buffer->setHasAlpha(!hasAlpha);
+    
+    gdipBitmap->UnlockBits(&lockedBitmapData);
+}
+
+bool GDIPlusReader::decodeBMP(bool onlySize)
+{
+    // Set our size.
+    if (!m_parent->setSize(m_gdipBitmap->GetWidth(), m_gdipBitmap->GetHeight()))
+        return false;
+
+    if (onlySize)
+        return true;
+
+    // Initialize the framebuffer if needed.
+    ASSERT(m_buffer);  // Parent should set this before asking us to decode!
+    if (m_buffer->status() == ImageFrame::FrameEmpty) {
+        if (!m_buffer->setSize(m_parent->size().width(), m_parent->size().height()))
+            return m_parent->setFailed(); // Unable to allocate.
+        m_buffer->setStatus(ImageFrame::FramePartial);
+        // setSize() calls eraseARGB(), which resets the alpha flag, so we force
+        // it back to false here.  We'll set it true below in all cases where
+        // these 0s could actually show through.
+        m_buffer->setHasAlpha(false);
+
+        // For BMPs, the frame always fills the entire image.
+        m_buffer->setOriginalFrameRect(IntRect(IntPoint(), m_parent->size()));
+    }
+
+    fillbitmap(m_gdipBitmap, m_buffer);
+    m_buffer->setStatus(ImageFrame::FrameComplete);
+
+    return true;
+}
+
+} // blink
