da0542059f8e54d90c025e68c5a06643c0bc3496
冉朋 <22249030@qq.com>
2017-01-06 14:48:40 +0800

    添加node对应的lib库,方便后续修改

---
+++ node/lib/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// This module is soft-deprecated. Users should be directed towards using
+// the specific constants exposed by the individual modules on which they
+// are most relevant.
+const constants = process.binding('constants');
+Object.assign(exports,
+              constants.os.errors,
+              constants.os.signals,
+              constants.fs,
+              constants.crypto);
---
+++ node/lib/internal/process/promises.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const promiseRejectEvent = process._promiseRejectEvent;
+const hasBeenNotifiedProperty = new WeakMap();
+const promiseToGuidProperty = new WeakMap();
+const pendingUnhandledRejections = [];
+let lastPromiseId = 1;
+
+exports.setup = setupPromises;
+
+function setupPromises(scheduleMicrotasks) {
+  process._setupPromises(function(event, promise, reason) {
+    if (event === promiseRejectEvent.unhandled)
+      unhandledRejection(promise, reason);
+    else if (event === promiseRejectEvent.handled)
+      rejectionHandled(promise);
+    else
+      require('assert').fail(null, null, 'unexpected PromiseRejectEvent');
+  });
+
+  function unhandledRejection(promise, reason) {
+    hasBeenNotifiedProperty.set(promise, false);
+    promiseToGuidProperty.set(promise, lastPromiseId++);
+    addPendingUnhandledRejection(promise, reason);
+  }
+
+  function rejectionHandled(promise) {
+    const hasBeenNotified = hasBeenNotifiedProperty.get(promise);
+    if (hasBeenNotified !== undefined) {
+      hasBeenNotifiedProperty.delete(promise);
+      const uid = promiseToGuidProperty.get(promise);
+      promiseToGuidProperty.delete(promise);
+      if (hasBeenNotified === true) {
+        process.nextTick(function() {
+          if (!process.emit('rejectionHandled', promise)) {
+            const warning = new Error('Promise rejection was handled ' +
+                                      `asynchronously (rejection id: ${uid})`);
+            warning.name = 'PromiseRejectionHandledWarning';
+            warning.id = uid;
+            process.emitWarning(warning);
+          }
+        });
+      }
+
+    }
+  }
+
+  function emitPendingUnhandledRejections() {
+    let hadListeners = false;
+    while (pendingUnhandledRejections.length > 0) {
+      const promise = pendingUnhandledRejections.shift();
+      const reason = pendingUnhandledRejections.shift();
+      if (hasBeenNotifiedProperty.get(promise) === false) {
+        hasBeenNotifiedProperty.set(promise, true);
+        const uid = promiseToGuidProperty.get(promise);
+        if (!process.emit('unhandledRejection', reason, promise)) {
+          const warning = new Error('Unhandled promise rejection ' +
+                                    `(rejection id: ${uid}): ${reason}`);
+          warning.name = 'UnhandledPromiseRejectionWarning';
+          warning.id = uid;
+          process.emitWarning(warning);
+        } else {
+          hadListeners = true;
+        }
+      }
+    }
+    return hadListeners;
+  }
+
+  function addPendingUnhandledRejection(promise, reason) {
+    pendingUnhandledRejections.push(promise, reason);
+    scheduleMicrotasks();
+  }
+
+  return emitPendingUnhandledRejections;
+}
