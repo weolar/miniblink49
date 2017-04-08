8d55f14b4b50963b7eb80ff920111503540cb518
weolar <weolar@qq.com>
2017-04-08 17:55:35 +0800

    * 修复传入字符串为空时的crash

--- base/strings/string_util.cc
+++ base/strings/string_util.cc
@@ -78,6 +78,8 @@ std::wstring UTF8ToWide(const std::string& utf8) {
 //     return output;
     std::wstring utf16;
     size_t n = ::MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), nullptr, 0);
+    if (0 == n)
+        return L"";
     std::vector<wchar_t> wbuf(n);
     MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), &wbuf[0], n);
     utf16.resize(n);
@@ -104,6 +106,8 @@ std::string UTF16ToASCII(const string16& utf16) {
 std::string WideToUTF8(const string16& utf16) {
     std::string utf8;
     size_t n = ::WideCharToMultiByte(CP_ACP, 0, utf16.c_str(), -1, NULL, 0, NULL, NULL);
+    if (0 == n)
+        return "";
     std::vector<char> buf(n + 1);
     ::WideCharToMultiByte(CP_ACP, 0, utf16.c_str(), -1, &buf[0], n, NULL, NULL);
     utf8.resize(n);
