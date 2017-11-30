e77a8449da309efa68358ef058cdf2187fed4edb
weolar <weolar@qq.com>
2017-11-30 18:01:08 +0800

    * 补充点新版curl需要的头文件

---
+++ vc6/include/crt/stdbool.h
@@ -0,0 +1,26 @@
+//
+// stdbool.h
+//
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// The C Standard Library <stdbool.h> header.
+//
+#ifndef _STDBOOL
+#define _STDBOOL
+
+#define __bool_true_false_are_defined	1
+
+#ifndef __cplusplus
+
+#define bool	_Bool
+#define false	0
+#define true	1
+
+#endif /* __cplusplus */
+
+#endif /* _STDBOOL */
+
+/*
+ * Copyright (c) 1992-2010 by P.J. Plauger.  ALL RIGHTS RESERVED.
+ * Consult your license regarding permissions and restrictions.
+V5.30:0009 */
