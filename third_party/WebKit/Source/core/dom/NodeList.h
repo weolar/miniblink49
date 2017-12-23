35f2d9815e995d049f43089a579a8efebccda269
weolar <weolar@qq.com>
2017-12-23 12:54:44 +0800

    * 增加NodeList的功能，方便实现NodeList::forEachForBinding，这个会用在v8导出函数里。querySelectorAll返回的就是NodeList，新h5标准需要forEach等函数

--- third_party/WebKit/Source/core/dom/NodeList.h
+++ third_party/WebKit/Source/core/dom/NodeList.h
@@ -24,6 +24,7 @@
 #ifndef NodeList_h
 #define NodeList_h
 
+#include "bindings/core/v8/Iterable.h"
 #include "bindings/core/v8/ScriptWrappable.h"
 #include "core/CoreExport.h"
 #include "wtf/RefCounted.h"
@@ -32,7 +33,7 @@ namespace blink {
 
 class Node;
 
-class CORE_EXPORT NodeList : public RefCountedWillBeGarbageCollectedFinalized<NodeList>, public ScriptWrappable {
+class CORE_EXPORT NodeList : public RefCountedWillBeGarbageCollectedFinalized<NodeList>, public ScriptWrappable, public ValueIterable<Node*> {
     DEFINE_WRAPPERTYPEINFO();
 public:
     virtual ~NodeList() { }
@@ -51,7 +52,44 @@ public:
 
 protected:
     NodeList() { }
+
+private:
+    IterationSource* startIteration(ScriptState*, ExceptionState&) override;
 };
+
+namespace {
+
+class NodeListIterationSource final
+    : public ValueIterable<Node*>::IterationSource {
+public:
+    explicit NodeListIterationSource(NodeList& nodeList) : m_nodeList(nodeList) {}
+
+    bool next(ScriptState* scriptState,
+        Node*& value,
+        ExceptionState& exceptionState) override
+    {
+        value = m_nodeList->item(m_index);
+        return !!value;
+    }
+
+    DEFINE_INLINE_VIRTUAL_TRACE()
+    {
+        visitor->trace(m_nodeList);
+        ValueIterable<Node*>::IterationSource::trace(visitor);
+    }
+
+private:
+    const Member<NodeList> m_nodeList;
+};
+
+}  // namespace
+
+inline ValueIterable<Node*>::IterationSource* NodeList::startIteration(
+    ScriptState*,
+    ExceptionState&)
+{
+    return new NodeListIterationSource(*this);
+}
 
 } // namespace blink
 
