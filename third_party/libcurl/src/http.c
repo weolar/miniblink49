e0508a213be6cbc793564fc614f705369d59cccd
weolar <weolar@qq.com>
2018-01-08 23:06:37 +0800

    * 修复https://8894f.com/打不开的bug。原因是此网站返回的请求头是http1.0，但实际必须使用http1.1。 xsoft找到的问题原因。现在比较少网站还在用1.0了，但目前还不敢把所有curl的回应都改成1.1，所以目前只改https的强制走1.1

--- third_party/libcurl/src/http.c
+++ third_party/libcurl/src/http.c
@@ -1496,6 +1496,8 @@ CURLcode Curl_http_done(struct connectdata *conn,
 static bool use_http_1_1plus(const struct Curl_easy *data,
                              const struct connectdata *conn)
 {
+  if (conn->port == 443 || conn->ssl[0].use) // ssl use http1.1
+    return TRUE;
   if((data->state.httpversion == 10) || (conn->httpversion == 10))
     return FALSE;
   if((data->set.httpversion == CURL_HTTP_VERSION_1_0) &&
