f35387d80fe1b11a867d881499702783d544fedb
weolar <weolar@qq.com>
2018-02-02 17:41:32 +0800

    * 修复关闭内存缓存下，网易的顶部图标无法显示的bug。原因是此图标是	background-image: url(products_sprites0727.svg),none;这种形式， 而svg在RawResource::preCacheSubstituteDataForMainResource里被缓存准备加载的。现在对所有主资源类型的请求都放行

--- third_party/WebKit/Source/core/fetch/MemoryCache.cpp
+++ third_party/WebKit/Source/core/fetch/MemoryCache.cpp
@@ -175,7 +175,7 @@ MemoryCache::ResourceMap* MemoryCache::ensureResourceMap(const String& cacheIden
 void MemoryCache::add(Resource* resource)
 {
 #if ENABLE_WKE
-    if (!g_wkeMemoryCacheEnable && WTF::kNotFound == resource->url().string().find("_miniblink__data_"))
+    if (!g_wkeMemoryCacheEnable && Resource::MainResource != resource->type())
         return;
 #endif
 
