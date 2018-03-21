3bb2a3769d1f9354d4f9da45023085eaaa3f652a
weolar <weolar@qq.com>
2018-03-21 16:32:35 +0800

    * internal_bootstrap_node_native里的js代码需要加入asar支持

--- node/src/node_javascript.cc
+++ node/src/node_javascript.cc
@@ -3,6 +3,11 @@
 #include "v8.h"
 #include "env.h"
 #include "env-inl.h"
+#include "bootstrap_node_js.h"
+
+namespace content {
+    void readJsFile(const wchar_t* path, std::vector<char>* buffer);
+}
 
 namespace node {
 
@@ -13,11 +18,26 @@ using v8::Object;
 using v8::String;
 
 Local<String> MainSource(Environment* env) {
-  return String::NewFromUtf8(
-      env->isolate(),
-      reinterpret_cast<const char*>(internal_bootstrap_node_native),
-      NewStringType::kNormal,
-      sizeof(internal_bootstrap_node_native)).ToLocalChecked();
+    return String::NewFromUtf8(
+        env->isolate(),
+        reinterpret_cast<const char*>(internal_bootstrap_node_native_hook),
+        NewStringType::kNormal,
+        15869).ToLocalChecked();
+
+//     std::vector<char>* buffer = new std::vector<char>();
+//     content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\bootstrap_node.js", buffer);
+//     
+//     return String::NewFromUtf8(
+//         env->isolate(),
+//         reinterpret_cast<const char*>(&buffer->at(0)),
+//         NewStringType::kNormal,
+//         buffer->size()).ToLocalChecked();
+
+//     return String::NewFromUtf8(
+//         env->isolate(),
+//         reinterpret_cast<const char*>(internal_bootstrap_node_native),
+//         NewStringType::kNormal,
+//         sizeof(internal_bootstrap_node_native)).ToLocalChecked();
 }
 
 void DefineJavaScript(Environment* env, Local<Object> target) {
@@ -25,6 +45,12 @@ void DefineJavaScript(Environment* env, Local<Object> target) {
 
   for (auto native : natives) {
     if (native.source != internal_bootstrap_node_native) {
+//       if (0 == strcmp(native.name, "bootstrap_node")) {
+//           std::vector<char>* buffer = new std::vector<char>();
+//           content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\bootstrap_node.js", buffer);
+//           native.source = (const unsigned char*)&buffer->at(0);
+//           native.source_len = buffer->size();
+//       }
       Local<String> name = String::NewFromUtf8(env->isolate(), native.name);
       Local<String> source =
           String::NewFromUtf8(
