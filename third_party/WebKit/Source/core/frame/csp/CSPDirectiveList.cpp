593fb4ff35e62b5f42b7a39daa06c0826421c1e7
weolar <weolar@qq.com>
2018-07-04 11:42:19 -0500

    * http://ueditor.baidu.com/website/onlinedemo.html点击论坛，会提示cancel。原因就是这些安全策略。现在统一根据标志决定关闭与否

--- third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
+++ third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
@@ -385,21 +385,29 @@ bool CSPDirectiveList::allowBaseURI(const KURL& url, ContentSecurityPolicy::Redi
 
 bool CSPDirectiveList::allowChildContextFromSource(const KURL& url, ContentSecurityPolicy::RedirectStatus redirectStatus, ContentSecurityPolicy::ReportingStatus reportingStatus) const
 {
+    if (!RuntimeEnabledFeatures::cspCheckEnabled())
+        return true;
     return reportingStatus == ContentSecurityPolicy::SendReport ? checkSourceAndReportViolation(operativeDirective(m_childSrc.get()), url, ContentSecurityPolicy::ChildSrc, redirectStatus) : checkSource(operativeDirective(m_childSrc.get()), url, redirectStatus);
 }
 
 bool CSPDirectiveList::allowAncestors(LocalFrame* frame, const KURL& url, ContentSecurityPolicy::ReportingStatus reportingStatus) const
 {
+    if (!RuntimeEnabledFeatures::cspCheckEnabled())
+        return true;
     return reportingStatus == ContentSecurityPolicy::SendReport ? checkAncestorsAndReportViolation(m_frameAncestors.get(), frame, url) : checkAncestors(m_frameAncestors.get(), frame);
 }
 
 bool CSPDirectiveList::allowScriptNonce(const String& nonce) const
 {
+    if (!RuntimeEnabledFeatures::cspCheckEnabled())
+        return true;
     return checkNonce(operativeDirective(m_scriptSrc.get()), nonce);
 }
 
 bool CSPDirectiveList::allowStyleNonce(const String& nonce) const
 {
+    if (!RuntimeEnabledFeatures::cspCheckEnabled())
+        return true;
     return checkNonce(operativeDirective(m_styleSrc.get()), nonce);
 }
 
@@ -410,6 +418,8 @@ bool CSPDirectiveList::allowScriptHash(const CSPHashValue& hashValue) const
 
 bool CSPDirectiveList::allowStyleHash(const CSPHashValue& hashValue) const
 {
+    if (!RuntimeEnabledFeatures::cspCheckEnabled())
+        return true;
     return checkHash(operativeDirective(m_styleSrc.get()), hashValue);
 }
 
