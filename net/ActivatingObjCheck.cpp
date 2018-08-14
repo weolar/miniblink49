bcef115976502181f9afeaa3effade2251d2c3e1
weolar <405360001@qq.com>
2018-08-14 12:41:57 +0800

    * ActivatingObjCheck::doGarbageCollected(bool forceGC)里少了个leave

--- net/ActivatingObjCheck.cpp
+++ net/ActivatingObjCheck.cpp
@@ -78,9 +78,10 @@ void ActivatingObjCheck::doGarbageCollected(bool forceGC)
 {
     if (!forceGC) {
         ::EnterCriticalSection(&m_mutex);
-        if (m_activatingObjs->size() < 10)
-            return;
+        int size = m_activatingObjs->size();
         ::LeaveCriticalSection(&m_mutex);
+        if (size < 10)
+            return;
     }
 
     ::EnterCriticalSection(&m_mutex);
