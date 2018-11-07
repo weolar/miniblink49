547b1a63bd76012ba02a457d02f71d1bf914cb47
Gergul <mail0668@gmail.com>
2018-11-07 17:03:34 +0800

    x64位下改为窗口类型

--- wkexe/main.cpp
+++ wkexe/main.cpp
@@ -1,15 +1,11 @@
 #include "app.h"
 
-#ifndef _WIN64
 int WINAPI WinMain(
     __in HINSTANCE hInstance,
     __in_opt HINSTANCE hPrevInstance,
     __in LPSTR lpCmdLine,
     __in int nShowCmd
     )
-#else
-int main()
-#endif
 {
 	wkeInitialize();
 	{
