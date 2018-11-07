3ee57a0b01f643bf018615ecda57ef5c1b1ff973
Gergul <mail0668@gmail.com>
2018-11-07 16:06:44 +0800

    自动选择vs2015命令行编译环境

--- build/build_ALL.bat
+++ build/build_ALL.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
+@For /F "Skip=1" %%i in ('Wmic Logicaldisk Where "DriveType=3" Get Name') Do call "%%i\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
 set VisualStudioVersion=14.0
 devenv miniblink.sln /build "Debug|Win32" /Out ../out/Debug/build.log
 devenv miniblink.sln /build "Release|Win32" /Out ../out/Release/build.log
