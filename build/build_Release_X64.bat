9840d7b9ed842041e0cb1d463f863da016385259
Gergul <mail0668@gmail.com>
2018-11-12 20:17:30 +0800

    优化启动环境命令行

--- build/build_Release_X64.bat
+++ build/build_Release_X64.bat
@@ -1,4 +1,4 @@
-@For /F "Skip=1" %%i in ('Wmic Logicaldisk Where "DriveType=3" Get Name') Do call "%%i\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
+call "%VS140COMNTOOLS%\..\..\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
 set VisualStudioVersion=14.0
 devenv miniblink.sln /build "Release|x64" /Out ../out/Release/build.log /Project miniblink
 
