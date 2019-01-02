9bea48a8d6316d63210167104cab64184937208a
weolar <405360001@qq.com>
2019-01-02 12:47:48 +0800

    * 增加Utf8ToMByte

--- third_party/WebKit/Source/wtf/text/WTFStringUtil.cpp
+++ third_party/WebKit/Source/wtf/text/WTFStringUtil.cpp
@@ -148,7 +148,7 @@ void stringTrim(String& stringInOut, bool leftTrim, bool rightTrim)
     }
 }
 
-void MByteToWChar(LPCSTR lpcszStr, DWORD cbMultiByte, std::vector<UChar>* out, UINT codePage)
+void MByteToWChar(const char* lpcszStr, size_t cbMultiByte, std::vector<UChar>* out, UINT codePage)
 {
     out->clear();
 
@@ -163,7 +163,7 @@ void MByteToWChar(LPCSTR lpcszStr, DWORD cbMultiByte, std::vector<UChar>* out, U
     MultiByteToWideChar(codePage, 0, lpcszStr, cbMultiByte, &out->at(0), dwMinSize);
 }
 
-void WCharToMByte(LPCWSTR lpWideCharStr, DWORD cchWideChar, std::vector<char>* out, UINT codePage)
+void WCharToMByte(const wchar_t* lpWideCharStr, size_t cchWideChar, std::vector<char>* out, UINT codePage)
 {
     out->clear();
 
@@ -176,6 +176,28 @@ void WCharToMByte(LPCWSTR lpWideCharStr, DWORD cchWideChar, std::vector<char>* o
 
     // Convert headers from ASCII to Unicode.
     WideCharToMultiByte(codePage, 0, lpWideCharStr, cchWideChar, &out->at(0), dwMinSize, NULL, FALSE);
+}
+
+void Utf8ToMByte(const char* lpUtf8CharStr, size_t cchUtf8Char, std::vector<char>* out, UINT codePage)
+{
+    out->resize(0);
+
+    std::vector<UChar> tempBuf;
+    MByteToWChar(lpUtf8CharStr, cchUtf8Char, &tempBuf, CP_UTF8);
+    if (0 == tempBuf.size())
+        return;
+    WCharToMByte(&tempBuf[0], tempBuf.size(), out, codePage);
+}
+
+void MByteToUtf8(const char* lpMCharStr, size_t cchMChar, std::vector<char>* out, UINT codePage)
+{
+    out->resize(0);
+
+    std::vector<UChar> tempBuf;
+    MByteToWChar(lpMCharStr, cchMChar, &tempBuf, codePage);
+    if (0 == tempBuf.size())
+        return;
+    WCharToMByte(&tempBuf[0], tempBuf.size(), out, CP_UTF8);
 }
 
 bool splitStringToVector(const String& strData, const char strSplit, bool needTrim, WTF::Vector<String>& out)
@@ -227,46 +249,46 @@ std::string WTFStringToStdString(const WTF::String& str)
 
 bool isTextUTF8(const char *str, int length)
 {
-	int i = 0;
-	DWORD nBytes = 0; // UFT8可用1-6个字节编码,ASCII用一个字节
-	UCHAR chr = 0;
-	bool bAllAscii = true; // 如果全部都是ASCII, 说明不是UTF-8
-	for (i = 0; i < length; i++) {
-		chr = (UCHAR) * (str + i);
+    int i = 0;
+    DWORD nBytes = 0; // UFT8可用1-6个字节编码,ASCII用一个字节
+    UCHAR chr = 0;
+    bool bAllAscii = true; // 如果全部都是ASCII, 说明不是UTF-8
+    for (i = 0; i < length; i++) {
+        chr = (UCHAR)* (str + i);
 
-		if ((chr & 0x80) != 0) // 判断是否ASCII编码,如果不是,说明有可能是UTF-8,ASCII用7位编码,但用一个字节存,最高位标记为0,o0xxxxxxx
-			bAllAscii = false;
+        if ((chr & 0x80) != 0) // 判断是否ASCII编码,如果不是,说明有可能是UTF-8,ASCII用7位编码,但用一个字节存,最高位标记为0,o0xxxxxxx
+            bAllAscii = false;
 
-		if (nBytes == 0) { // 如果不是ASCII码,应该是多字节符,计算字节数
-		
-			if (chr >= 0x80) {
-				if (chr >= 0xFC && chr <= 0xFD)
-					nBytes = 6;
-				else if (chr >= 0xF8)
-					nBytes = 5;
-				else if (chr >= 0xF0)
-					nBytes = 4;
-				else if (chr >= 0xE0)
-					nBytes = 3;
-				else if (chr >= 0xC0)
-					nBytes = 2;
-				else {
-					return false;
-				}
-				nBytes--;
-			}
-		} else { // 多字节符的非首字节,应为 10xxxxxx
-			if ((chr & 0xC0) != 0x80)
-				return false;
-			nBytes--;
-		}
-	}
-	if (nBytes > 0) //违返规则
-		return false;
+        if (nBytes == 0) { // 如果不是ASCII码,应该是多字节符,计算字节数
 
-	if (bAllAscii) //如果全部都是ASCII, 说明不是UTF-8
-		return false;
-	return true;
+            if (chr >= 0x80) {
+                if (chr >= 0xFC && chr <= 0xFD)
+                    nBytes = 6;
+                else if (chr >= 0xF8)
+                    nBytes = 5;
+                else if (chr >= 0xF0)
+                    nBytes = 4;
+                else if (chr >= 0xE0)
+                    nBytes = 3;
+                else if (chr >= 0xC0)
+                    nBytes = 2;
+                else {
+                    return false;
+                }
+                nBytes--;
+            }
+        } else { // 多字节符的非首字节,应为 10xxxxxx
+            if ((chr & 0xC0) != 0x80)
+                return false;
+            nBytes--;
+        }
+    }
+    if (nBytes > 0) //违返规则
+        return false;
+
+    if (bAllAscii) //如果全部都是ASCII, 说明不是UTF-8
+        return false;
+    return true;
 }
 
 
--- third_party/WebKit/Source/wtf/text/WTFStringUtil.h
+++ third_party/WebKit/Source/wtf/text/WTFStringUtil.h
@@ -15,12 +15,14 @@ Vector<char> ensureStringToUTF8(const String& string, bool isNullTermination);
 String ensureStringToUTF8String(const String& string);
 std::string WTFStringToStdString(const WTF::String& str);
 
-bool isTextUTF8(const char *str, int length);
+bool isTextUTF8(const char* str, int length);
 
 bool splitStringToVector(const String& strData, const char strSplit, bool needTrim, WTF::Vector<String>& out);
 
-void MByteToWChar(LPCSTR lpcszStr, DWORD cbMultiByte, std::vector<UChar>* out, UINT codePage);
-void WCharToMByte(LPCWSTR lpWideCharStr, DWORD cchWideChar, std::vector<char>* out, UINT codePage);
+void MByteToWChar(const char* lpcszStr, size_t cbMultiByte, std::vector<UChar>* out, UINT codePage);
+void WCharToMByte(const wchar_t* lpWideCharStr, size_t cchWideChar, std::vector<char>* out, UINT codePage);
+void Utf8ToMByte(const char* lpUtf8CharStr, size_t cchUtf8Char, std::vector<char>* out, UINT codePage);
+void MByteToUtf8(const char* lpMCharStr, size_t cchMChar, std::vector<char>* out, UINT codePage);
 
 }
 
