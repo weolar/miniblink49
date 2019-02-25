169d5c160d592199bbbb071edb27bf199a1e6235
weolar <405360001@qq.com>
2019-02-25 18:06:04 +0800

    * 整理代码

--- node/src/node_javascript.cc
+++ node/src/node_javascript.cc
@@ -3,11 +3,6 @@
 #include "v8.h"
 #include "env.h"
 #include "env-inl.h"
-#include "bootstrap_node_js.h"
-
-namespace content {
-void readJsFile(const wchar_t* path, std::vector<char>* buffer);
-}
 
 namespace node {
 
@@ -20,18 +15,9 @@ using v8::String;
 Local<String> MainSource(Environment* env) {
     return String::NewFromUtf8(
         env->isolate(),
-        reinterpret_cast<const char*>(internal_bootstrap_node_native_hook),
+        reinterpret_cast<const char*>(internal_bootstrap_node_native),
         NewStringType::kNormal,
-        15869).ToLocalChecked();
-
-//     std::vector<char>* buffer = new std::vector<char>();
-//     content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\bootstrap_node.js", buffer);
-// 
-//     return String::NewFromUtf8(
-//         env->isolate(),
-//         reinterpret_cast<const char*>(&buffer->at(0)),
-//         NewStringType::kNormal,
-//         buffer->size()).ToLocalChecked();
+        sizeof(internal_bootstrap_node_native)).ToLocalChecked();
 }
 
 void DefineJavaScript(Environment* env, Local<Object> target) {
@@ -39,35 +25,6 @@ void DefineJavaScript(Environment* env, Local<Object> target) {
 
     for (auto native : natives) {
         if (native.source != internal_bootstrap_node_native) {
-
-//             if (0 == strcmp(native.name, "internal/child_process")) {
-//                 std::vector<char>* buffer = new std::vector<char>();
-//                 content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\child_process.js", buffer);
-//                 native.source = (const unsigned char*)&buffer->at(0);
-//                 native.source_len = buffer->size();
-//             }
-
-            if (0 == strcmp(native.name, "url")) {
-                std::vector<char>* buffer = new std::vector<char>();
-                content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\url.js", buffer);
-                native.source = (const unsigned char*)&buffer->at(0);
-                native.source_len = buffer->size();
-            }
-
-            if (0 == strcmp(native.name, "internal/url")) {
-                std::vector<char>* buffer = new std::vector<char>();
-                content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\url.js", buffer);
-                native.source = (const unsigned char*)&buffer->at(0);
-                native.source_len = buffer->size();
-            }
-
-            if (0 == strcmp(native.name, "internal/querystring")) {
-                std::vector<char>* buffer = new std::vector<char>();
-                content::readJsFile(L"E:\\mycode\\miniblink49\\trunk\\node\\lib\\internal\\querystring.js", buffer);
-                native.source = (const unsigned char*)&buffer->at(0);
-                native.source_len = buffer->size();
-            }
-
             Local<String> name = String::NewFromUtf8(env->isolate(), native.name);
             Local<String> source =
                 String::NewFromUtf8(
