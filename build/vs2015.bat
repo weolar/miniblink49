48c955732ced2c73d3a4122475ff6d893614b297
冉朋 <22249030@qq.com>
2019-04-28 18:49:47 +0800

    批处理

--- build/vs2015.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
-set VisualStudioVersion=14.0
-devenv miniblink.sln
--- build/vs2019.bat
+++ build/vs2019.bat
@@ -1,5 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat" %2
 set VisualStudioVersion=16.0
-devenv miniblink.sln /build "Debug|Win32" /Out ../out/Release/build.log /Project miniblink
-
-pause
+devenv miniblink.sln
