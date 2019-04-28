a25116185cb137c8f1cd76c2674f9aec6fa59c65
冉朋 <22249030@qq.com>
2019-04-29 00:07:11 +0800

    编译脚本

--- build/build_Release_X64.bat
+++ build/build_Release_X64.bat
@@ -1,5 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat" %2
-set VisualStudioVersion=14.0
-devenv miniblink.sln /build "Release|x64" /Out ../out/Release/build.log /Project miniblink
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat" %2
+set VisualStudioVersion=16.0
+devenv miniblink.sln /Project "miniblink" /build "Release|x64" /Out ../out/Release/build.log /Project miniblink
 
 pause
