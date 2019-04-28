3608d2d95280be0609c078404aa012cab5bfd02e
冉朋 <22249030@qq.com>
2019-04-28 21:14:31 +0800

    工程配置

--- build/build_ALL.bat
+++ build/build_ALL.bat
@@ -1,8 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat" %2
 set VisualStudioVersion=16.0
-devenv miniblink.sln /build "Debug|Win32" /Out ../out/Debug/build.log
-devenv miniblink.sln /build "Release|Win32" /Out ../out/Release/build.log
-devenv miniblink.sln /build "Release_vc6|Win32" /Out ../out/Release_vc6/build.log
-devenv miniblink.sln /build "Debug|x64" /Out ../out/x64/Debug/build.log
-devenv miniblink.sln /build "Release|x64" /Out ../out/x64/Release/build.log
+devenv miniblink.sln /Project "miniblink" /build "Debug|Win32" /Out ../out/Debug/build.log
+devenv miniblink.sln /Project "miniblink" /build "Release|Win32" /Out ../out/Release/build.log
+devenv miniblink.sln /Project "miniblink" /build "Release_vc6|Win32" /Out ../out/Release_vc6/build.log
+devenv miniblink.sln /Project "miniblink" /build "Debug|x64" /Out ../out/x64/Debug/build.log
+devenv miniblink.sln /Project "miniblink" /build "Release|x64" /Out ../out/x64/Release/build.log
 pause
